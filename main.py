c2eb7079d0e5d8128ea5bff1ec5cb9e690363fc5
Desktop-Andy <andyredx@qq.com>
2024-12-12 11:25:14 +0800

    feat: 添加命令行参数以支持短浏览和长浏览模式

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import asyncio
+import argparse
 from DrissionPage import Chromium, ChromiumOptions
 from autobrowser import wait_for_new_topics, process_topic
 
 async def main():
+    # 添加命令行参数解析
+    parser = argparse.ArgumentParser(description='浏览 linux.do 主题')
+    parser.add_argument('-m', '--mode', choices=['short', 'long'], default='short',
+                       help='浏览模式: short(浏览10个主题) 或 long(浏览所有主题)')
+    args = parser.parse_args()
+
     # 设置浏览器路径
     browser_paths = {
         'chrome': r'C:\Program Files\Google\Chrome\Application\chrome.exe',
@@ -36,20 +43,22 @@ async def main():
     browser = Chromium(co)
     tab = browser.new_tab('https://linux.do/')
     topic_list_body = tab('.topic-list-body')
-
-    # 循环加载更多主题
-    while True: 
-        topic_list = topic_list_body.eles('t=tr')
-        end_topic_id = topic_list[-1].attr('id')
-        if len(topic_list) > 50:
-            break
-        try:
-            # 尝试加载更多主题
-            wait_for_new_topics(tab, end_topic_id)
-        except TimeoutError:
-            # 如果超时，说明没有更多主题了，退出循环
-            print('没有更多主题加载，结束程序')
-            break
+    
+    if args.mode == 'short':
+        # 短浏览模式: 直接获取前10个主题
+        topic_list = topic_list_body.eles('t=tr')[:10]
+    else:
+        # 长浏览模式: 循环加载更多主题
+        while True:
+            topic_list = topic_list_body.eles('t=tr') 
+            end_topic_id = topic_list[-1].attr('id')
+            if len(topic_list) > 50:
+                break
+            try:
+                wait_for_new_topics(tab, end_topic_id)
+            except TimeoutError:
+                print('没有更多主题加载，结束程序')
+                break
 
     semaphore = asyncio.Semaphore(3)
     tasks = [process_topic(topic, n, semaphore) for n, topic in enumerate(topic_list)]
